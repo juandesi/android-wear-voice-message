704e67a793ab6c630d47747c8e935f616146f3dd
JuanDesi <juan.desimoni@mulesoft.com>
2015-04-25 09:49:33 -0300

    updated .gitignore, renamed activities and deleted idea files

---
+++ mobile/src/main/java/com/example/android/wearablemessageapiexample/MobileActivity.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2015 Dejan Djurovski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.wearablemessageapiexample;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class MobileActivity extends Activity {
+    private static final String BASE_URL = "http://hackaton.cloudhub.io/addMovement?name=";
+    private final String COMMAND_PATH = "/command";
+
+    private GoogleApiClient apiClient;
+    private NodeApi.NodeListener nodeListener;
+    private MessageApi.MessageListener messageListener;
+    private String command;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        // Create MessageListener that receives messages sent from a wearable
+        messageListener = new MessageApi.MessageListener() {
+            @Override
+            public void onMessageReceived(MessageEvent messageEvent) {
+                if (messageEvent.getPath().equals(COMMAND_PATH)) {
+
+                    command = new String(messageEvent.getData());
+
+
+                    HttpClient httpclient = new DefaultHttpClient();
+
+                    try {
+                        httpclient.execute(new HttpGet());
+                    }catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        };
+
+        // Create GoogleApiClient
+        apiClient = new GoogleApiClient.Builder(getApplicationContext()).addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+            @Override
+            public void onConnected(Bundle bundle) {
+                // Register Node and Message listeners
+                Wearable.NodeApi.addListener(apiClient, nodeListener);
+                Wearable.MessageApi.addListener(apiClient, messageListener);
+                // If there is a connected node, get it's id that is used when sending messages
+            }
+
+            @Override
+            public void onConnectionSuspended(int i) {
+            }
+        }).addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(ConnectionResult connectionResult) {
+                if (connectionResult.getErrorCode() == ConnectionResult.API_UNAVAILABLE)
+                    Toast.makeText(getApplicationContext(), getString(R.string.wearable_api_unavailable), Toast.LENGTH_LONG).show();
+            }
+        }).addApi(Wearable.API).build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Check is Google Play Services available
+        int connectionResult = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+
+        if (connectionResult != ConnectionResult.SUCCESS) {
+            // Google Play Services is NOT available. Show appropriate error dialog
+            GooglePlayServicesUtil.showErrorDialogFragment(connectionResult, this, 0, new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    finish();
+                }
+            });
+        } else {
+            apiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        // Unregister Node and Message listeners, disconnect GoogleApiClient and disable buttons
+        Wearable.NodeApi.removeListener(apiClient, nodeListener);
+        Wearable.MessageApi.removeListener(apiClient, messageListener);
+        apiClient.disconnect();
+        super.onPause();
+    }
+}
